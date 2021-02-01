c1888862aef186061c0b6c28412d6ae55fa0f251
Gertjan De Wilde <info@gertjandewilde.com>
2021-02-01 20:38:31 +0100

    Update domain

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'https://brrrfund.money';
+export const SITE_URL = 'https://brrr.fund';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'wsbmod';
 export const BRAND_NAME = 'BRRR Fund';
